3226c6601a889ae95ec68ed78db497db4427f9a6
David A. Ball <david@daball.me>
2015-07-23 02:30:22 -0400

    php-prototype branch contains only php-prototype code

--- daball-codecombat-prototype/codecombat-zork-basic-game/js/gameConfiguration.js
+++
@@ -1,6 +0,0 @@
-angular.module('GameConfigurationModule', [])
-  .value('debug', true) //set this to true to enable debug features, false to disable
-  .value('appName', "Rowdy Red\'s Java Adventure")
-  .value('userServiceAPIUrl', "http://localhost/user-api.php")
-  .value('promptDisplay', '> ')
-  .value('promptSecretChar', '\u25CF')
--- daball-codecombat-prototype/codecombat-zork-basic-game/js/homeUIController.js
+++
@@ -1,5 +0,0 @@
-//injects ui.ace and MapServiceModule modules
-angular.module('GameUIControllerModule', ['GameConfigurationModule', 'UserServiceModule'])
-  //this is the game UI controller, injects MapService, debug, appName, and GameEngine services
-  .controller('GameUIController', function($scope, $state, debug, appName, MapService, GameEngine) {
-  });
