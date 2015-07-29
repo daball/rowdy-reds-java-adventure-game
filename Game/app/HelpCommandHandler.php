893980732e9bbdb5e0fdaca081a1bcb3f46112de
David Ball <daball@users.noreply.github.com>
2015-07-29 04:02:58 -0400

    Improved HelpCommandHandler.php readability.

--- Game/app/HelpCommandHandler.php
+++ Game/app/HelpCommandHandler.php
@@ -21,30 +21,30 @@ class HelpCommandHandler extends CommandHandlerInterface
   public function executeCommand($gameState, $commandLine)
   {
     $eol = "\n";
-    return "********* HELP MENU *********" . $eol .
-           $eol .
-           "Goal: Explore our tiny castle." . $eol .
-           $eol .
-           "HELP displays this help screen." . $eol .
-           $eol .
-           "Restart the game by typing:" . $eol .
-           "restart or reset" . $eol .
-           $eol .
-           "Exit the game by typing:" . $eol .
-           "exit or System.exit(0);" . $eol .
-           $eol .
-           "Move north by typing:" . $eol .
-           "north, n, or moveNorth();" . $eol .
-           $eol .
-           "Move south by typing:" . $eol .
-           "south, s, or moveSouth();" . $eol .
-           $eol .
-           "Move west by typing:" . $eol .
-           "west, w, moveWest();" . $eol .
-           $eol .
-           "Move east by typing:" . $eol .
-           "east, e, moveEast();" . $eol .
-           $eol .
-           "********** END HELP **********";
+    return  "********* HELP MENU *********" . $eol
+          . $eol
+          . "Goal: Explore our tiny castle." . $eol
+          . $eol
+          . "HELP displays this help screen." . $eol
+          . $eol
+          . "Restart the game by typing:" . $eol
+          . "restart or reset" . $eol
+          . $eol
+          . "Exit the game by typing:" . $eol
+          . "exit or System.exit(0);" . $eol
+          . $eol
+          . "Move north by typing:" . $eol
+          . "north, n, or moveNorth();" . $eol
+          . $eol
+          . "Move south by typing:" . $eol
+          . "south, s, or moveSouth();" . $eol
+          . $eol
+          . "Move west by typing:" . $eol
+          . "west, w, moveWest();" . $eol
+          . $eol
+          . "Move east by typing:" . $eol
+          . "east, e, moveEast();" . $eol
+          . $eol
+          . "********** END HELP **********";
   }
 }
