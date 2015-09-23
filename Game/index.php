8c9bb198b6ad3a61218aef30aa501fdfbd4bad1d
David A. Ball <david@daball.me>
2015-09-22 23:14:46 -0400

    Resolve 3-way merge

--- Game/index.php
+++ Game/index.php
@@ -1,77 +1,47 @@
 <?php
-	if(!isset($_POST['screen'])){
-		$screen = 1;
-	}
-	else{
-		$screen = $_POST['screen'];
-	}
 
-	if(isset($_POST['ans'])){
-		$command = trim(substr($_POST['ans'], strrpos($_POST['ans'], '> ') + 1));
-		include 'rules.php';
-	}
+require_once __DIR__.'/app/game/GameEngine.php';
 
-	if(!isset($returnAnswer)){
-		$returnAnswer = "> ";
-	}
+//start session services
+session_start();
 
-	$roomImages = Array
-	(
-			1 => "background.jpg",
-			2 => "mainHall.jpg",
-			3 => "CastleRoom.jpg",
-			20 => "exitScreen.jpg"
-	);
-?>
+//start the game engine
+$gameEngine = new GameEngine();
 
-<html>
-	<head>
-		<title>Rowdy Red's Java Adventure</title>
-		<link href="./css/style.css" rel="stylesheet" type="text/css" />
-		<script src="./vendor/components/jquery/jquery.min.js"></script>
-		<script>
-			$(function() {
-			  $("[autofocus]").on("focus", function() {
-				if (this.setSelectionRange) {
-				  var len = this.value.length * 2;
-				  this.setSelectionRange(len, len);
-				} else {
-				  this.value = this.value;
-				}
-				this.scrollTop = 999999;
-			  }).focus();
-			});
-		</script>
-	</head>
-	<body>
-		<div style="width: 1350px; height: auto; margin-top: 25px;">
-			<img style="margin-left: 0px; float: left;" src="images/light.png" />
-			<div id="screen" style="background-image: url('images/<?php echo $roomImages[$screen]; ?>'); background-size:100%; background-repeat: no-repeat;">
-			</div>
-			<?php
-			if(!isset($consoleHidden))
-			{
-				?>
+//install shortcut variables, for easier to read code
+$eol = "\n";
 
-			<div id="holder">
-				<form method="post" id="answerForm" name="answerForm">
-					<input style="display: none;" type="text" name="screen" value="<?php echo $screen; ?>" />
-					<textarea autofocus id="ans" name="ans" style="color: #eee; background-color: black; height: 535px;" spellcheck="false" autocorrect="false" autocapitalize="false" onkeydown="if (event.keyCode == 13) {document.getElementById('button').click();} if (event.keyCode == 8 ) {var id = document.getElementById('ans').value; var check = id.substr(id.length - 2) + id.substr(id.length - 1); if(check == '>  '){ return false;} } if(event.keyCode == 46 || event.keyCode == 37 || event.keyCode == 38 || event.keyCode == 39 || event.keyCode == 40 ) { return false;} "><?php
-						if(isset($_POST['ans'])){
-							echo $returnAnswer;
-						}
-						else{
-						 echo "You are standing at a castle door. A door lies to the north.\n> ";
-						}
-					?></textarea>
+$gameState = $gameEngine->gameState;
+$map = $gameState->map;
 
-					<input id="button" hidden type="submit" value="Enter Command" />
-				</form>
-			</div>
-			<?php
-			}
-			?>
-			<img style="margin-left: 230; float: left;" src="images/light.png" />
-		</div>
-	</body>
-</html>
+$avatarRoom = $gameState->getAvatarRoom();
+$moves = $gameState->moves;
+$isExiting = $gameState->isExiting;
+
+$roomName = $avatarRoom->name;
+$imageUrl = $avatarRoom->imageUrl;
+
+$consoleHistory = $gameState->consoleHistory;
+$commandProcessor = $gameEngine->commandProcessor;
+$commandInput = $commandProcessor->commandInput;
+$commandOutput = $commandProcessor->commandOutput;
+
+
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem(__DIR__.'/views');
+$twig = new Twig_Environment($loader, array(
+    'cache' => __DIR__.'/tmp',
+));
+$template = $twig->loadTemplate('index.html');
+echo $template->render(array('the' => 'variables', 'go' => 'here'));
+
+//when the game is exiting, go ahead and restart it, since there is no other way to restart the session
+//next time it loads, it'll be ready to play
+if ($isExiting) {
+	//reset game state
+	$gameEngine->gameState = new GameState();
+}
+
+//always save the session state
+$gameEngine->saveSession();
