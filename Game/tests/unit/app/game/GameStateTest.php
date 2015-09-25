17ec60bf14a494f4486e1cd7164ead5adc6835b2
David A. Ball <david@daball.me>
2015-09-24 22:11:09 -0400

    Updated GameStateTest for refactored GameState->avatarLocation to Player->location

--- Game/tests/unit/app/game/GameStateTest.php
+++ Game/tests/unit/app/game/GameStateTest.php
@@ -10,25 +10,25 @@ class GameStateTest extends PHPUnit_Framework_TestCase
   {
     $gameState = new GameState();
 
-    $this->assertEquals($gameState->map->getSpawnPoint(), $gameState->avatarLocation);
-    $this->assertEquals($gameState->map->getRoom($gameState->map->getSpawnPoint())->name, $gameState->avatarLocation);
+    $this->assertEquals($gameState->map->getSpawnPoint(), $gameState->getPlayer()->location);
+    $this->assertEquals($gameState->map->getRoom($gameState->map->getSpawnPoint())->name, $gameState->getPlayer()->location);
     $this->assertContains("Game started.", $gameState->consoleHistory);
     $this->assertContains($gameState->inspectRoom(), $gameState->consoleHistory);
 
     //test inspect room
-    $this->assertEquals($gameState->inspectRoom(), $gameState->map->getRoom($gameState->avatarLocation)->description);
-    $this->assertEquals($gameState->avatarLocation, $gameState->getAvatarRoom()->name);
+    $this->assertEquals($gameState->inspectRoom(), $gameState->map->getRoom($gameState->getPlayer()->location)->description);
+    $this->assertEquals($gameState->getPlayer()->location, $gameState->getAvatarRoom()->name);
     $this->assertEquals($gameState->inspectRoom(), $gameState->getAvatarRoom()->description);
 
     //move the character's location, one room to the north
-    $initialRoom = $gameState->avatarLocation;
+    $initialRoom = $gameState->getPlayer()->location;
     $gameState->navigate('north');
-    $this->assertNotEquals($initialRoom, $gameState->avatarLocation);
+    $this->assertNotEquals($initialRoom, $gameState->getPlayer()->location);
 
     //reset the game state
     $gameState->resetGameState();
-    $this->assertEquals($gameState->map->getSpawnPoint(), $gameState->avatarLocation);
-    $this->assertEquals($gameState->map->getRoom($gameState->map->getSpawnPoint())->name, $gameState->avatarLocation);
+    $this->assertEquals($gameState->map->getSpawnPoint(), $gameState->getPlayer()->location);
+    $this->assertEquals($gameState->map->getRoom($gameState->map->getSpawnPoint())->name, $gameState->getPlayer()->location);
     $this->assertContains("Game restarted.", $gameState->consoleHistory);
   }
 }
