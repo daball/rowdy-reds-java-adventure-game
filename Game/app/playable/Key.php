41b6ebf3d30ad33ed932bf6a3c9fb9adfae5c3af
David A. Ball <david@daball.me>
2015-09-27 16:14:30 -0400

    Include IInspectable to Key

--- Game/app/playable/Key.php
+++ Game/app/playable/Key.php
@@ -3,6 +3,7 @@
 namespace playable;
 
 require_once __DIR__.'/../util/ISerializable.php';
+require_once __DIR__.'/../util/IInspectable.php';
 
 /**
  * A Key item must be used to open an Unlockable item.
