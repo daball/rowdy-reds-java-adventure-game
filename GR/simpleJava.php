67d2be6b2588a0f7215907a94b02e0f7cb5118c9
David A. Ball <david@daball.me>
2015-11-01 22:15:31 -0500

    Commented line 33 in simpleJava.php which assigned Java tablet code to command recall feature

--- GR/simpleJava.php
+++ GR/simpleJava.php
@@ -1,20 +1,20 @@
 <?php
 	include 'helper.php';
-	
+
 	$_SESSION['tText'] = $_POST['tabHistory'];
 	$command = trim($_SESSION['tText']);
-	
+
 	function runCode($javaCode)
 	{
 		if($_SESSION['CurrentRoom'] == "goldilocksRoom")
 		{
 			$wayOne = 'if(bed.equals("hard")){fluffPillow=true;}elseif(bed.equals("soft")){eatCookies=true;}else{bed="justright";}';
 			$wayTwo = 'if(bed.equals("soft")){eatCookies=true;}elseif(bed.equals("hard")){fluffPillow=true;}else{bed="justright";}';
-			
-			
+
+
 			$javaCode = str_replace(' ', '', $javaCode);
 			$javaCode = preg_replace( "/\r|\n/", "", $javaCode);
-			
+
 			if(strpos($javaCode, $wayOne) !== false || strpos($javaCode, $wayTwo) !== false){
 				consoleAppend("You wrote the correct Java Code and made it past the goldilocksRoom");
 			}
@@ -22,11 +22,15 @@
 				consoleAppend("Error in code try again.");
 			}
 		}
+		else {
+			//need some output for any other condition
+			consoleAppend("You wrote some Java code in the tablet and nothing happened.");
+		}
 	}
 
 	runCode($command);
-	
-	$_SESSION['lastCommand'] = $command;
+
+	// $_SESSION['lastCommand'] = $command;
 	header("Location: main.php");
 	exit;
 ?>
