c587a76e6d12e78e39807a45e220bff77f7bbfdc
David A. Ball <david@daball.me>
2015-11-28 03:32:15 -0500

    Fix: Use platform-independent directory separator in PHP, remove leading \

--- app/java/TabletCompilerService.php
+++ app/java/TabletCompilerService.php
@@ -17,7 +17,7 @@ class TabletCompilerService {
   public function __construct() {
     $cachePath = Path::combine(__DIR__, "..", "..", "__player_tablet_cache");
     if (DIRECTORY_SEPARATOR != "/")
-      $cachePath = str_replace("/", DIRECTORY_SEPARATOR, $cachePath);
+      $cachePath = trim(str_replace("/", DIRECTORY_SEPARATOR, $cachePath), "\\");
     $this->javaCompilerService = new java("edu.radford.rowdyred.internal.TabletCompilerService", session_id(), $cachePath, null);
   }
 
