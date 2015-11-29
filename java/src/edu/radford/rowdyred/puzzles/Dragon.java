bcaad02e1944c4a5ec77ce6cfbbe4bab0b470764
Sean Rider <rommager@cox.net>
2015-11-29 14:01:29 -0500

    Implemented dragon battle pattern

--- java/src/edu/radford/rowdyred/puzzles/Dragon.java
+++ java/src/edu/radford/rowdyred/puzzles/Dragon.java
@@ -2,12 +2,15 @@ package edu.radford.rowdyred.puzzles;
 
 public class Dragon extends Enemy {
 
-	private boolean flying, inhaling;
+	private boolean flying, inhaling, engaged;
+	private String[] attackPattern ={ "fire","tail","fly","fire","land" };
+	private int turnNumber;
 	
 	public Dragon(int initialHealth) {
 		super(initialHealth);
 		flying = false;
 		inhaling = false;
+		turnNumber = 0;
 	}
 	
 	public boolean isFlying() {
@@ -18,19 +21,38 @@ public class Dragon extends Enemy {
 		return inhaling;
 	}
 	
-/*	public String nextTurn() {
-		StringBuilder sb = new StringBuilder();
-		if (!isFlying && !isInhaling) {
-			sb.append("The dragon takes to the air!\n");
-		}
-		else if (isInhaling) {			
-			sb.append("The dragon starts inhaling!\n");
-		}
-		else if ()
-		sb.append("The dragon lands on the ground!\n");
-		return sb.toString();
+	public void engage() {
+	  engaged = true;
 	}
-*/
+	
+	public String nextTurn() {
+	  if (!engaged)
+	    return "The dragon doesn't seem to notice you.";
+	  String response = "";
+	  switch (attackPattern[turnNumber]) {
+	  case "fire":
+	    inhaling = true;
+	    response = "The dragon breathes fire at you!";
+	    break;
+	  case "tail":
+	    response = "The dragon whips its tail at you!";
+      break;
+	  case "fly":
+	    flying = true;
+	    response = "The dragon takes flight!";
+      break;
+    case "land":
+      flying = false;
+      response = "The dragon lands back on the ground!";
+      break;
+	  }    
+	  
+		turnNumber++;
+		if (turnNumber > attackPattern.length -1)
+		  turnNumber = 0;
+		return response;
+	}
+
 	
 	public void Attack (Player player) {
 		
--- java/src/edu/radford/rowdyred/puzzles/tests/DragonFightTester.java
+++ java/src/edu/radford/rowdyred/puzzles/tests/DragonFightTester.java
@@ -4,12 +4,17 @@ import edu.radford.rowdyred.puzzles.*;
 
 public class DragonFightTester {
 
-  public static void main(String args[]) {
+  public static void main(String[] args) {
     Dragon dragon = new Dragon(1000);
     Player player = new Player(100);
     Salve salve = new Salve();
     Weapon sword = new Weapon(75, true);
     Shield shield = new Shield(55, 90);
     Weapon crossbow = new Weapon(35, false);
+    
+    dragon.engage();
+    for (int i = 1; i <= 10; i++)
+    System.out.println(dragon.nextTurn());
+    
   }
 }
