c9ec2d41484e3735eef911ef6e30e2f67e5645f0
Sean Rider <rommager@cox.net>
2015-11-29 02:26:13 -0500

    Adding some backpack logic to game builder

--- app/game/GameBuilder.php
+++ app/game/GameBuilder.php
@@ -96,6 +96,9 @@ function assembleItemsIntoContainer($roomDefinition, $gameObject, $items) {
       case 'generalObject':
         $container->insertItem(assembleGeneralObject($roomDefinition, $gameObject, $item));
         break;
+//      case 'backpack':
+//        $container->insertItem(assembleGeneralObject($roomDefinition, $gameObject, $item));
+//        break;
     }
   }
   return $container;
