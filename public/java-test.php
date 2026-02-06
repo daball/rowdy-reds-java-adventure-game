735f1cab81625019e0359d2bdfbc5bf4c1ce142e
David Ball <david@daball.me>
2026-02-06 17:12:47 -0500

    fix: update service URLs to match Docker services

--- public/java-test.php
+++ public/java-test.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("http://localhost:8081/cc-development/java/Java.inc");
+require_once("http://tomcat:8080/cc-development/java/Java.inc");
 
 $Player = new java("edu.radford.rowdyred.game.Player");
 echo $Player->inspect();
